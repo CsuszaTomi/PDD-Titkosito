9b192a1874a137e6f84c9659488eec031b5d88e3
PappDD <PappDD@301-11>
2025-10-16 11:02:28 +0200

    Add project files.

---
+++ Titkosito/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Titkosito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string abc = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz";
+            int kulcs = 4263;
+            Console.Write("Add meg a szöveget amit titkosítani szeretnél: ");
+            string szoveg = Console.ReadLine();
+            string titkositott = "";
+
+        }
+    }
+}
